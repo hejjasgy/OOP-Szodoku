f0a5ee9dea6e1d23765c77a0aa5ed72c25b8dba3
hgyula <hejjasgy@kkszki.hu>
2023-09-11 11:33:17 +0200

    Kész

--- Program.cs
+++ Program.cs
@@ -71,18 +71,46 @@ namespace sudokuCLI{
 
         static void negyedikFeladat(int meretX){
             int talatMeret = 0;
+            
+            List<Feladvany> feladvanysz = new List<Feladvany>();
 
             foreach (Feladvany feladvany in feladvanyLIST){
                 if(feladvany.Meret == meretX){
                     talatMeret++;
+                    feladvanysz.Add(feladvany);
 
                 }
             }
 
 
             
-            Console.WriteLine(meretX+"x"+meretX + " méretű feladványból " + talatMeret + " darab van tárolva");
+            Console.WriteLine(meretX+"x"+meretX + " méretű feladványból " + talatMeret + " darab van tárolva\n");
+      
+            Feladvany veletlenFeladvany = feladvanysz[new Random().Next(0, feladvanysz.Count - 1)];
+
+            Console.WriteLine("5. feladat: A kiválaszott feladvány:\n" + veletlenFeladvany.Kezdo);
+
+            int kitoltottMezok = 0;
+            double osszesMezo = veletlenFeladvany.Kezdo.Length;
+
+            foreach(char karakter in veletlenFeladvany.Kezdo){
+                if(karakter != '0') kitoltottMezok++;
+            }
+
+            double szazalek = (kitoltottMezok/osszesMezo)*100;
+            Console.WriteLine("6. feladat: A feladvány kitöltöttsége: " + Math.Round(szazalek,0)+"%\n");
+            Console.WriteLine("7. feladat: A feladvány kirajzolva:");
+            veletlenFeladvany.Kirajzol();
+
+            StreamWriter streamWriter = new StreamWriter("sudoku" + meretX + ".txt");
+            foreach (Feladvany feladvany in feladvanysz){
+                streamWriter.WriteLine(feladvany.Kezdo);
+            }
+            streamWriter.Flush();
+            streamWriter.Close();
             
+            Console.WriteLine("8. feladat: sudoku" + meretX + ".txt állomány " + feladvanysz.Count + " darab feladvánnyal létrehozva");
+
         }
     }
 }
